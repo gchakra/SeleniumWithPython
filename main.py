64fe79b5ad10d4b8fc6cda7f8389d2f7a96e58e0
GOPAL CHAKRAVARTHY <cg1790@gmail.com>
2022-11-20 13:27:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -19,66 +19,82 @@ import difflib
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.webdriver import WebDriver
+
+# from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
+
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.support import expected_conditions as EC
 
 # import xmltojson
-# import json
 # import requests
 
-url = "http://localhost:8080/gopalc/addnumbers.html"
+baseurl = "http://localhost:8080/gopalc/addnumbers.html"
+#myxpaths = {
+#    "txtNum1": "//input[@name='txtNum1']",
+#    "txtNum2": "//input[@name='txtNum2']",
+#    "txtSum": "//input[@name='txtSum']",
+#    "Submit": "//input[@name='Submit']"
+#}
 
 
 def foo(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f"ProgramName, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
-    i = 10
+    # print(f"ProgramName, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+    i = 12
     j = 15
     k = i + j
 
-    # driver = webdriver.Chrome("C:/apps/python/chromedriver.exe")
+    # json_objects = json.dumps(myxpaths, indent=4)
+    # print(json_objects)
 
-    domain = urlparse(url).netloc
-    print("domain " + domain)
-    driver: WebDriver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get(url)
+    # with open("fields.json", "w") as outfile:
+    # json.dump(myxpaths, outfile)
+
+    with open("fields.json", "r") as read_it:
+        myxpaths = json.load(read_it)
+    print(myxpaths)
+    # domain = urlparse(baseurl).netloc
+    # print("domain " + domain)
+    driver: webdriver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install())
+    )
+    driver.get(baseurl)
     driver.maximize_window()
 
     # time.sleep(5)  # Let the user actually see something!
-    print("Page tile " + driver.title)
-    content = driver.find_elements(By.XPATH,"/html/body")
-    print("Content")
-    print(content)
-    txtNum1: WebElement = driver.find_element(By.ID, "txtNum1")
+    # print("Page tile " + driver.title)
+    # content = driver.find_elements(By.XPATH,"/html/body")
+    # login_form = driver.find_element_by_xpath("/html/body/form[1]")
+    # print("Content")
+    # print(content)
+    # txtNum1: WebElement = driver.find_element(By.ID, "txtNum1")
+
+    txtSum: WebElement = driver.find_element(By.XPATH, value=myxpaths["txtSum"])
+    txtSum.clear()
+    txtNum1: WebElement = driver.find_element(By.XPATH, value=myxpaths["txtNum1"])
+    txtNum1.clear()
+    txtNum2: WebElement = driver.find_element(By.XPATH, value=myxpaths["txtNum2"])
+    txtNum2.clear()
     txtNum1.send_keys(str(i))
-    print("txtNum 1 : " + txtNum1.get_attribute('value'))
-    txtNum2: WebElement = driver.find_element(By.ID, "txtNum2")
     txtNum2.send_keys(str(j))
-    print("txtNum 2 : " + txtNum2.get_attribute('value'))
-    Submit: WebElement = driver.find_element(By.ID,"Submit")
+    Submit: WebElement = driver.find_element(By.XPATH, value=myxpaths["Submit"])
     Submit.click()
-    txtSum: WebElement = driver.find_element(By.ID, "txtSum")
-    print("txtSum : " + txtSum.get_attribute('value'))
+    print("txtNum 1 : " + txtNum1.get_attribute("value"))
+    print("txtNum 2 : " + txtNum2.get_attribute("value"))
+    print("txtSum : " + txtSum.get_attribute("value"))
     print("Expected Value :")
     print(k)
     # Save the page content as sample.html
     with open("sample.html", "w") as html_file:
         html_file.write(txtNum1.text)
-
-
-    # elem2 = driver.find_element(By.NAME, "txtNum2")
-    # print(elem2)
-    # elem.clear()
-    # elem.send_keys("pycon")
-    # elem.send_keys(Keys.RETURN)
-    # assert "No results found." not in driver.page_source
     driver.close()
 
 
